06f05708da0aa363ddc36dbb8c7d4ebd1803c2a9
Wilmar Guevara <wguevarac@unadvirtual.edu.co>
2016-09-20 23:49:46 -0500

    pagina en la web 20-09-16
    se sube la pagina para terminar la actividad dos

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="es" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta content="Wilmar Guevara" name="author"/>
+	<meta content="Pagina Ova 1" name="description"/>
+	<meta content="Unad, Ova, taller" name="keyword"/>
+	<title>Ova 01</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+<header>
+    <div id="main-logo"> <img src="img/logo.png"  width="30%"></div>
+	 <div id="main-texto"><a href="index.php">OBJETO VIRTUAl DE APRENDIZAJE PARA LA INICIACION EN PAGINAS WEB</a></div>
+</header>
+
+<nav>
+	<ul id="menu">
+		<li><a href="#">Inicio</a></li>
+		<li><a href="#">Lecturas</a></li>
+		<li><a href="#">Multimedia</a></li>							
+        <li><a href="#">Actividades</a></li>
+		<li><a href="#">Autores</a></li>
+	</ul>
+
+</nav>
+<section>
+	<hgrupo>
+		<H1>Actividad No.2 <br>Creando la Plantilla para un OVA	con	<b>HTML5 y	CSS3</b>.</H1>
+		<H3>Introducion</H3>
+	</hgrupo>
+	<p>A.	
+	
+El	 estudiante	 creará	 un	 sitio	 web	
+utilizando	 lenguajes	 de	 diseño	 como	
+HTML5,	 CSS3	 y	 el	 cual	 servirá	 como	
+plantilla	para	el	diseño	y	publicación	de	
+un	Objeto	Virtual	de	Aprendizaje	OVA,	
+con	base	a	los	parámetros	y	elementos	
+establecidos	 en	 la	 guía	 integrada	 de	
+actividades	del	curso.</p>
+<br>
+<h2>Nueva Plantilla HTML5</h2>
+<BR>
+<div id="imagen"><img src="img/plantilla.png"></div>
+
+
+
+</section>
+
+<aside>
+<hgrupo>
+		<H1>Recomendaciones Hechas Por El Docente:</H1>
+		<H3>Introducion</H3>
+	</hgrupo>
+	<p>Estimados estudiantes el desarrollo de actividades colaborativas en ambientes virtuales de aprendizaje demanda dos características importantes en cada 
+uno de ustedes. <b>La primera es la disciplina</b>, ya que la participación activa en esta actividad demanda una disciplina en el orden de dedicar por lo menos
+unas horas al desarrollo de la actividad y no unos minutos antes del cierre de la misma. <b>La segunda característica hace referencia a la ética</b>  para el
+desarrollo de actividades, ya que ustedes en su rol de estudiantes formándose integralmente deben cumplir ética y moralmente con el desarrollo de las
+actividades, el plagio es un delito y es penalizado por la universidad por lo cual se le invita desde ya a que explote su creatividad, sus ideas y pensamientos
+con productos originales y de su autoría.</p>
+
+</aside>
+
+<footer>
+
+   <p> Version inicial para el estudio de la plantilla en html5</p>
+   <h3>Premiera version pagina web<br> ____wilmar guevara cabrera_____20-09-16.</h3>
+
+</footer>
+
+
+
+</body>
+
+
+<script src="js/modernizr-custom.js"></script>
+<script src="js/prefixfree.min.js"></script>
+<script src="js/jquery-3.1.0.min.js"></script>
+
+</html>
